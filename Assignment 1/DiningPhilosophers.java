1a535020bfe52fde06c7f342f4a69334939e6883
Niftalemm <112132272+Niftalemm@users.noreply.github.com>
2024-02-11 23:43:12 -0600

    Update DiningPhilosophers.java

--- Assignment 1/DiningPhilosophers.java
+++ Assignment 1/DiningPhilosophers.java
@@ -1,16 +1,16 @@
-public class DiningPhilosophers {
+ublic class DiningPhilosophers {
 
   public static void main(String[] args) throws InterruptedException {
-    Philosopher[] philosophers = new Philosopher[5];
-    Chopstick[] chopsticks = new Chopstick[5];
+
+    Philosopher[] philosophers = new Philosopher[5]; // The number of philosophers
+    Chopstick[] chopsticks = new Chopstick[5]; // The number of Chopsticks
     
     for (int i = 0; i < 5; ++i)
-      chopsticks[i] = new Chopstick(i);
+      chopsticks[i] = new Chopstick(i); //creats chopsticks
     for (int i = 0; i < 5; ++i) {
-      philosophers[i] = new Philosopher(chopsticks[i], chopsticks[(i + 1) % 5]);
-      philosophers[i].start();
+      philosophers[i] = new Philosopher(i, chopsticks[i], chopsticks[(i + 1) % 5]);
+      philosophers[i].start(); // Philospher amount
     }
     for (int i = 0; i < 5; ++i)
       philosophers[i].join();
   }
-}
