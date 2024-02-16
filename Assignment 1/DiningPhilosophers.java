01ecb7510f3abeb42d3f4fc4bee8e317a687021e
Niftalem <niftalem.kassa@mnsu.edu>
2024-02-16 17:44:44 -0600

    Create the variables

--- Assignment 1/DiningPhilosophers.java
+++ Assignment 1/DiningPhilosophers.java
@@ -2,8 +2,22 @@ public class DiningPhilosophers {
 
   public static void main(String[] args) throws InterruptedException {
 
-    Philosopher[] philosophers = new Philosopher[5]; // The number of philosophers
-    Chopstick[] chopsticks = new Chopstick[5]; // The number of Chopsticks
+    String stringNp = args[0];  //Number of philosohpers
+    String stringNc = args[1];  //Number of cycles
+    String stringTt = args[2];  //Maximum thinking time
+    String stringEt = args[3];  //Maximum eating time
+    String stringRl = args[4];  //If 0, all right handed. If 1, then even # philosophers are right handed and odd # are left.
+
+    //coverting to Int to make it easier
+    int np = Integer.parseInt(stringNp);
+    int nc = Integer.parseInt(stringNc);
+    int tt = Integer.parseInt(stringTt);
+    int et = Integer.parseInt(stringEt);
+    int rl = Integer.parseInt(stringRl);
+
+
+    Philosopher[] philosophers = new Philosopher[np]; // The number of philosophers
+    Chopstick[] chopsticks = new Chopstick[np]; // The number of Chopsticks
     
     for (int i = 0; i < 5; ++i)
       chopsticks[i] = new Chopstick(i); //creats chopsticks
